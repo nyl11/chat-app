570de163282e84fc4d93bee8680af1afb60caf97
nyl11 <khadkanoyal@gmail.com>
2026-03-11 16:53:45 +0545

    feat: Add health check endpoint and serve frontend static assets in production.

--- ChatApp-backend/src/index.js
+++ ChatApp-backend/src/index.js
@@ -3,16 +3,19 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 
-import {connectDB} from "./lib/db.js"
+import { connectDB } from "./lib/db.js"
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
 
-const PORT=process.env.PORT;
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
@@ -23,10 +26,24 @@ app.use(cors(
     }
 ))
 
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-server.listen(PORT, ()=>{
-    console.log('server is running on port:'+ PORT)
+//health check
+app.get("/health", (req, res) => {
+    res.send("OK");
+})
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../ChatApp-frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../ChatApp-frontend", "dist", "index.html"));
+    });
+}
+
+server.listen(PORT, () => {
+    console.log('server is running on port:' + PORT)
     connectDB();
 })
